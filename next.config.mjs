b22f914bd38272658b373afcc85ac93ad6c4c6c4
PepeTheProgrammer69 <ytytyt456789@gmail.com>
2025-07-16 03:34:56 +0500

    Fixed the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://archlinux.tail9023a4.ts.net/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://archlinux.tail9023a4.ts.net/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
